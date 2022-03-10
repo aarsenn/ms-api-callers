b713e9d40dfff422745ae08c7625ebfb6a26c986
aarsenn <arsenterletsky@gmail.com>
2022-03-10 20:05:50 +0200

    added logs for request, response and error

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
 
 const APP_ID_TYPES = {
   INHERIT_FROM_SESSION: 'inherit_from_session'
@@ -66,6 +67,14 @@ async function initApiCallers({ appId, msAuthAppSelectMethod, storage, context, 
       appUpdatedCallback(app);
     };
 
+    const hideToken = (requestOptions, replacer = 'TOKEN IS HIDDEN') => ({
+      ...requestOptions,
+      headers: {
+        ...requestOptions.headers,
+        Authorization: requestOptions.headers.Authorization ? replacer : requestOptions.headers.Authorization
+      }
+    });
+
     const checkTokens = () => {
       if (!app) return Promise.reject(new Error('No app providet in check tokens'));
       if (app.tokensExpired >= Date.now()) return Promise.resolve(app);
@@ -77,13 +86,13 @@ async function initApiCallers({ appId, msAuthAppSelectMethod, storage, context, 
         headers: { 'Authorization': `FLOW ${context.config.flowToken}` }
       };
 
-      context.log.info('Start tokens update', { app, callingFlow: authProviderUrl });
+      context.log.debug('Start MS Teams tokens refresh', { app, callingFlow: authProviderUrl });
 
       return axios(requestOptions).then(resp => {
         if (!resp || !resp.data) return null;
         app = resp.data;
         emitAppUpdated(app);
-        context.log.info('Tokens updated', { app });
+        context.log.debug('Tokens updated', { app });
         return resp.data;
       });
     };
@@ -94,6 +103,7 @@ async function initApiCallers({ appId, msAuthAppSelectMethod, storage, context, 
 
     const callGraph = opts => {
       const url = url => opts.version ? replVers(url, opts.version) : url;
+      const requestId = uuidv4();
       return checkTokens()
         .then(app => {
           const headers = { 'Authorization': `Bearer ${app.graphToken}` };
@@ -102,20 +112,28 @@ async function initApiCallers({ appId, msAuthAppSelectMethod, storage, context, 
             url: url(config.graphApiEndpoint) + opts.url,
           };
           const httpOpts = Object.assign(opts, graphOpts);
-          context.log.info('MS Graph http request', { request: httpOpts });
+
+          context.log.debug(`MS Graph request - ${requestId}`, hideToken(httpOpts));
+
           return axios(httpOpts);
         })
         .then(resp => opts.meta ? resp.data : (resp.data.value || resp.data))
+        .then(value => {
+          context.log.debug(`MS Graph response - ${requestId}`, value);
+          return value;
+        })
         .catch(err => {
           const requestError = () => ({ response: err.response.data, status: err.response.status });
           const error = () => ({ message: err.message || err });
-          context.log.error('Error on http request', err.response ? requestError() : error());
+          context.log.error(`Error on MS Graph request - ${requestId}`, err.response ? requestError() : error());
           throw err;
         });
     };
 
     const callBotframework = opts => {
       const url = url => opts.version ? replVers(url, opts.version) : url;
+      const requestId = uuidv4();
+
       return checkTokens()
         .then(app => {
           const headers = { 'Authorization': `Bearer ${app.botToken}` };
@@ -124,14 +142,18 @@ async function initApiCallers({ appId, msAuthAppSelectMethod, storage, context, 
             url: url(app.botServiceUrl || config.botDefaultApiEndpoint) + opts.url,
           };
           const httpOpts = Object.assign(opts, graphOpts);
-          context.log.info('MS Botframework http request', { request: httpOpts });
+          context.log.debug(`MS Botframework request - ${requestId}`, hideToken(httpOpts));
           return axios(httpOpts);
         })
         .then(resp => resp.data)
+        .then(value => {
+          context.log.debug(`MS Botframework response - ${requestId}`, value);
+          return value;
+        })
         .catch(err => {
           const requestError = () => ({ response: err.response.data, status: err.response.status });
           const error = () => ({ message: err.message || err });
-          context.log.error('Error on http request', err.response ? requestError() : error());
+          context.log.error(`Error on MS Botframework request - ${requestId}`, err.response ? requestError() : error());
           throw err;
         });
     };
